b84534ce923491e3f0934fc0b0ffee15aa4270bd
Krunali <krunalilole29@gmail.com>
2022-07-08 23:51:05 +0530

    Added:Microservices Project where we are developing userservice and contact service with the help of Eureka Server and API Gateway

---
+++ contactservice/contactservice/src/main/java/com/bridgelabz/contactservice/service/ContactService.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.contactservice.service;
+
+import com.bridgelabz.contactservice.entity.Contact;
+
+import java.util.List;
+
+public interface ContactService {
+
+    public List<Contact> getContactsOfUser(Long userId);
+}
